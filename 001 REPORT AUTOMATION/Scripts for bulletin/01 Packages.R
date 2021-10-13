79348dc36202e7f430012f916b661294614319b2
martinsielfeld <msielfeld@alumnos.uai.cl>
2021-10-12 21:26:30 -0300

    changes

--- 001 REPORT AUTOMATION/Scripts for bulletin/01 Packages.R
+++ 001 REPORT AUTOMATION/Scripts for bulletin/01 Packages.R
@@ -1,3 +1,4 @@
 
 pack <- c('data.table','dplyr','ggplot2','scales','flextable','officer','lubridate')
+install.packages(setdiff(pack, rownames(installed.packages())))
 sapply(pack,require,character.only=TRUE)
